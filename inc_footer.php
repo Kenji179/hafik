a07ad1184550e5e711332470d81c9a2d1cf973d1
Tomassv <tomas@swdesign.cz>
2016-04-29 11:18:41 +0800

    Added Google Analitics

--- inc_footer.php
+++ inc_footer.php
@@ -1,29 +1,49 @@
 <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    Štefánikova 102/7 - 284 01, Kutná Hora | Tel: 604 787 347 | Email: info@skolkahafik.cz
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                Štefánikova 102/7 - 284 01, Kutná Hora | Tel: 604 787 347 | Email: info@skolkahafik.cz
             </div>
         </div>
-    </footer>
-    <section class="bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    &copy; 2016 - <a href="http://www.karellen.cz/" target="_blank">Karellen</a>
-                </div>
+    </div>
+</footer>
+<section class="bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; 2016 - <a href="http://www.karellen.cz/" target="_blank">Karellen</a>
             </div>
         </div>
-    </section>
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/datetimepicker.js"></script>
-    <script src="assets/js/datetimepicker.cs.js"></script>
-    <script src="assets/js/validator.min.js"></script>
-    <script src="assets/js/fancybox.js"></script>
-    <script src="assets/js/custom.js"></script>
+    </div>
+</section>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/datetimepicker.js"></script>
+<script src="assets/js/datetimepicker.cs.js"></script>
+<script src="assets/js/validator.min.js"></script>
+<script src="assets/js/fancybox.js"></script>
+<script src="assets/js/custom.js"></script>
+
+<!-- Analytics -->
+
+<script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-62024970-10', 'auto');
+    ga('send', 'pageview');
+
+</script>
 
 </body>
 
