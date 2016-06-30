f416f74d0e9b09393e66d9eecb64f1590bad5977
Tomassv <tomas@swdesign.cz>
2016-06-30 23:09:17 +0000

    Pricing updated

--- cenik.php
+++ cenik.php
@@ -30,12 +30,130 @@ include('inc_header.php');
         <div class="row">
             <div class="col-md-12">
                 <h1>Ceník</h1>
-                <ul>
-                    <li>
-                        <h2>Základní částka za hlídání dětí je <strong>80,- Kč za 1 hodinu</strong> (dále se účtuje každá započatá půlhodina tzn. 40,- Kč).</h2></li>
-                    <li>
-                        <h2>Částka za přespávání je jednotná <strong>450,- Kč za dítě</strong> (dle předchozího objednání).</h2></li>
-                </ul>
+                <a href="docs/skolkovne.pdf" title="Školkovné" download="skolkovne.pdf"><i class="fa fa-file-pdf-o fa-fw"></i>&nbsp; CENÍK KE STAŽENÍ</a>
+            </div>
+            <div class="col-md-12">
+                <h2>Školkovné</h2>
+                <h3>Ceny docházky stálé skupiny za měsíc</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Program</th>
+                            <th>celodenní</th>
+                            <th>dopolední</th>
+                            <th>odpolední</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>provozní doba</td>
+                            <td>7:30 - 17:30</td>
+                            <td>7:30 - 12:30</td>
+                            <td>12:30 - 17:30</td>
+                        </tr>
+                        <tr>
+                            <td>1 den</td>
+                            <td>1 200 Kč</td>
+                            <td>800 Kč</td>
+                            <td>800 Kč</td>
+                        </tr>
+                        <tr>
+                            <td>2 dny</td>
+                            <td>2 100 Kč</td>
+                            <td>1 500 Kč</td>
+                            <td>1 500 Kč</td>
+                        </tr>
+                        <tr>
+                            <td>3 dny</td>
+                            <td>3 300 Kč</td>
+                            <td>2 100 Kč</td>
+                            <td>2 100 Kč</td>
+                        </tr>
+                        <tr>
+                            <td>4 dny</td>
+                            <td>3 700 Kč</td>
+                            <td>2 600 Kč</td>
+                            <td>2 600 Kč</td>
+                        </tr>
+                        <tr>
+                            <td>5 dní</td>
+                            <td>4 000 Kč</td>
+                            <td>2 900 Kč</td>
+                            <td>2 900 Kč</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>věk dětí:</td>
+                            <td>2 - 6 let</td>
+                        </tr>
+                        <tr>
+                            <td>kapacita:</td>
+                            <td>10 dětí</td>
+                        </tr>
+                        <tr>
+                            <td>strava:</td>
+                            <td>65 Kč/den zahrnuje oběd a 2x svačinku</td>
+                        </tr>
+                        <tr>
+                            <td>absence:</td>
+                            <td>náhrada nejpozději v následujícím měsíci nebo sleva 20% z ceny omluvených dní</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-12">
+                <h2>Ceny hlídání dětí pro veřejnost</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Hlídání</th>
+                            <th>celodenní</th>
+                            <th>dopolední</th>
+                            <th>odpolední</th>
+                            <th>hodina</th>
+                            <th>noční přespání</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>provozní doba</td>
+                            <td>7:30 - 17:30</td>
+                            <td>7:30 - 12:30</td>
+                            <td>12:30 - 17:30</td>
+                            <td>7:30 - 17:30</td>
+                            <td>18:30 - 9:00</td>
+                        </tr>
+                        <tr>
+                            <td>cena</td>
+                            <td>300 Kč</td>
+                            <td>200 Kč</td>
+                            <td>200 Kč</td>
+                            <td>80 Kč</td>
+                            <td>300 Kč</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>věk dětí:</td>
+                            <td>2 - 8 let</td>
+                        </tr>
+                        <tr>
+                            <td>kapacita:</td>
+                            <td>dle obsazenosti centra</td>
+                        </tr>
+                        <tr>
+                            <td>strava:</td>
+                            <td>po dohodě lze zajistit nebo podáme stravu donesenou</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
