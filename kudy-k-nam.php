cae9fa5eae9d4a51e4a1d2f376d94b91d9b1b175
Tomassv <tomas@swdesign.cz>
2016-08-28 00:17:48 +0000

    Fix to Kudy k nam

--- kudy-k-nam.php
+++ kudy-k-nam.php
@@ -38,7 +38,7 @@ include('inc_header.php');
                     <p>E-mail <strong>info@skolkahafik.cz</strong></p>
                     <p>Webové stránky <strong>skolkahafik.cz</strong></p>
                     <p>Obchodní firma <strong>Hafík – hlídání dětí s.r.o. (IČ: 04498518)</strong>
-                    <strong>Hafík - dětské centrum, z.s. (IČ: 05340063)</strong></p>
+                    <strong>Hafík - dětské centrum, z.s.<br>(IČ: 05340063)</strong></p>
                 </address>
                 <iframe src="//api.mapy.cz/frame?params=%7B%22x%22%3A15.27141402932302%2C%22y%22%3A49.9530164617787%2C%22base%22%3A%221%22%2C%22layers%22%3A%5B%5D%2C%22zoom%22%3A17%2C%22url%22%3A%22https%3A%2F%2Fmapy.cz%2Fs%2FCQCp%22%2C%22mark%22%3A%7B%22x%22%3A%2215.27141402932302%22%2C%22y%22%3A%2249.9530164617787%22%2C%22title%22%3A%22%C5%A0tef%C3%A1nikova%20102%2F7%2C%20Kutn%C3%A1%20Hora%22%7D%2C%22overview%22%3Afalse%7D&amp;width=100&amp;height=350" height="350" frameborder="0" style="border:0; width: calc(100% - 305px);" allowfullscreen></iframe>
             </div>
