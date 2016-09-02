0fc0d1dc229663dfe1cbcafb505e4defb1dac1ba
Roman Navratil <navratir@gmail.com>
2016-09-02 12:20:08 +0000

    use smtp for sending emails

--- EmailSender.php
+++ EmailSender.php
@@ -9,6 +9,14 @@ abstract class EmailSender
         $mail->CharSet = 'utf-8';
         $mail->isHTML(true);
 
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'roman@swdesign.cz';                 // SMTP username
+        $mail->Password = 'Ap[x(2e?bmepG5_';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;
+
         $mail->setFrom($from);
         $mail->addAddress($to);
         $mail->Subject = $subject;
